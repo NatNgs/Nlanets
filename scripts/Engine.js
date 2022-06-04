21c1db9d4be7cf585faba5c0559583416614d61a
Nathaël NOGUÈS <nathael.nogues@gmail.com>
2022-06-04 15:48:23 +0200

    Planets have name & Population

--- scripts/Engine.js
+++ scripts/Engine.js
@@ -2,135 +2,235 @@ const SCALING = 2**10
 
 var Engine = (function () {
 	const REPARTITION_REPEAT = 1
-
-	function Engine() {
-		this.planets = []
-		this.ships = []
-		this.width = 0
-		this.height = 0
-	}
-
 	const MIN_DISTANCE_BETWEEN_PLANETS = 1.5 * SCALING
-	Engine.prototype.set = function(width, height, planets, seed) {
-		this.width = width * SCALING
-		this.height = height * SCALING
-		this.planets = []
 
-		const rng = RND.newGenerator(seed)
+	class Engine {
+		constructor() {
+			this.planets = []
+			this.ships = []
+			this.width = 0
+			this.height = 0
+		}
+		set(width, height, planets, seed) {
+			this.width = width * SCALING
+			this.height = height * SCALING
+			this.planets = []
 
-		// Generate random planets
-		let retries = 999
-		while(this.planets.length < planets && retries > 0) {
-			const newP = new Planet(rng, this.width, this.height)
+			const rng = RND.newGenerator(seed)
 
-			// Check if planet is not intersecting any other planet
-			let intersect = false
-			for(const planet of this.planets) {
-				if (newP.distanceTo(planet) < MIN_DISTANCE_BETWEEN_PLANETS) {
-					intersect = true
-					break
+			// Generate random planets
+			let retries = 999
+			while (this.planets.length < planets && retries > 0) {
+				const newP = new Planet(rng, this.width, this.height)
+
+				// Check if planet is not intersecting any other planet
+				let intersect = false
+				for (const planet of this.planets) {
+					if (newP.distanceTo(planet) < MIN_DISTANCE_BETWEEN_PLANETS) {
+						intersect = true
+						break
+					}
+				}
+				if (!intersect) {
+					this.planets.push(newP)
+				} else {
+					retries--
 				}
 			}
-			if(!intersect) {
-				this.planets.push(newP)
-			} else {
-				retries --
+		}
+		onClick(xy, turn) {
+			// Find the planet at this location
+			const previouslySelectedPlanet = this.selectedPlanet
+			if (previouslySelectedPlanet) {
+				previouslySelectedPlanet.selected = false
+				this.selectedPlanet = null
 			}
-		}
-	}
 
-	Engine.prototype.onClick = function(xy, turn) {
-		// Find the planet at this location
-		const previouslySelectedPlanet = this.selectedPlanet
-		if(previouslySelectedPlanet) {
-			previouslySelectedPlanet.selected = false
-			this.selectedPlanet = null
-		}
-
-		for (const planet of this.planets) {
-			if(planet.containsLocation(xy)) {
-				if(previouslySelectedPlanet !== planet) {
-					if(previouslySelectedPlanet) {
-						// Send a ship from previously selected planet to this planet
-						this.ships.push(new Ship(previouslySelectedPlanet, planet, turn))
-					} else {
-						this.selectedPlanet = planet
-						planet.selected = true
+			for (const planet of this.planets) {
+				if (planet.containsLocation(xy)) {
+					if (previouslySelectedPlanet !== planet) {
+						if (previouslySelectedPlanet && previouslySelectedPlanet.population > 0) {
+							// Send a ship from previously selected planet to this planet
+							const population = (Math.random() * (previouslySelectedPlanet.population - 1) + 1) | 0
+							previouslySelectedPlanet.addPopulation(-population)
+							this.ships.push(new Ship(previouslySelectedPlanet, planet, turn, population))
+						} else {
+							this.selectedPlanet = planet
+							planet.selected = true
+						}
 					}
 				}
 			}
 		}
-	}
-
-	Engine.prototype.update = function(turn) {
-		for(const ship of this.ships) {
-			ship.update(turn)
-			if(ship.isArrived(turn)) {
-				this.ships.splice(this.ships.indexOf(ship), 1)
+		update(turn) {
+			for (const ship of this.ships) {
+				ship.update(turn)
+				if (ship.isArrived(turn)) {
+					this.ships.splice(this.ships.indexOf(ship), 1)
+					ship.toPlanet.addPopulation(ship.population)
+				}
 			}
 		}
-	}
+		render(graphics, animationTime) {
+			graphics.beginFill(0x080810)
+			graphics.drawRect(0, 0, this.width, this.height)
+			graphics.endFill()
 
-	Engine.prototype.render = function(graphics, animationTime) {
-		graphics.beginFill(0x080810)
-		graphics.drawRect(0, 0, this.width, this.height)
-		graphics.endFill()
-
-		for(const planet of this.planets) {
-			planet.render(graphics, animationTime)
+			for (const planet of this.planets) {
+				planet.render(graphics, animationTime)
+			}
+			for (const ship of this.ships) {
+				ship.render(graphics, animationTime)
+			}
+			return true
 		}
-		for(const ship of this.ships) {
-			ship.render(graphics, animationTime)
+		getBounds() {
+			return [{ x: 0, y: 0 }, { x: this.width, y: this.height }]
 		}
-		return true
 	}
 
 	Engine.prototype.TURNS_PER_SECOND = 1
 
-	Engine.prototype.getBounds = function() {
-		return [{x:0, y:0}, {x:this.width, y:this.height}]
-	}
-
 	return Engine
 })()
 
+
+function rgb2hsv(rgb) {
+	// Rgb = 0xRRGGBB
+	const r = (rgb >> 16)/0xFF, g = ((rgb >> 8) & 0xFF)/0xFF, b = (rgb & 0xFF)/0xFF
+
+	const v=Math.max(r,g,b)
+	const c=v-Math.min(r,g,b)
+	let h= c && ((v==r) ? (g-b)/c : ((v==g) ? 2+(b-r)/c : 4+(r-g)/c))
+	h = (h<0?h+6:h)/6
+	const s = v&&c/v
+
+	const hsv = ((h*0xFF)|0) << 16 | ((s*0xFF)|0) << 8 | (v*0xFF)|0
+
+	// return 0xHHSSVV
+	return hsv
+}
+
 var Planet = (function () {
-	function Planet(rng, width, height) {
-		this.x = rng.float(SCALING, width - SCALING)
-		this.y = rng.float(SCALING, height - SCALING)
-		this.size = rng.float(0.2, 0.8) * SCALING
-		this.color = rng.int(0, 0xFFFFFF)
-		this.selected = false
-	}
-
-	Planet.prototype.containsLocation = function(xy) {
-		const distance = Math.sqrt(Math.pow(this.x - xy.x, 2) + Math.pow(this.y - xy.y, 2))
-		return distance < this.size
-	}
-	Planet.prototype.distanceTo = function(planet) {
-		const distance = Math.sqrt(Math.pow(this.x - planet.x, 2) + Math.pow(this.y - planet.y, 2))
-		return distance - this.size - planet.size
-	}
-
 	const BORDER_SIZE = .25 * SCALING
-	Planet.prototype.render = function(graphics, time) {
-		graphics.beginFill(0x808080, this.selected ? 1 : .25)
-		graphics.drawCircle(this.x, this.y, this.size + BORDER_SIZE)
-		graphics.endFill()
+	const NAME_LETTERS = "ΓΔΘΛΞΠΣΦΨΩABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-		graphics.beginFill(this.color)
-		graphics.drawCircle(this.x, this.y, this.size)
-		graphics.endFill()
+	function generatePlanetName(values) {
+		let gen = ''
+
+		gen += NAME_LETTERS[(values.shift()*NAME_LETTERS.length) |0].toUpperCase()
+		for(const v of values) {
+			gen += NAME_LETTERS[(v*NAME_LETTERS.length) |0].toLowerCase()
+		}
+
+		return gen
+	}
+
+
+	class Planet {
+		constructor(rng, width, height) {
+			this.x = rng.float(SCALING, width - SCALING)
+			this.y = rng.float(SCALING, height - SCALING)
+			this.size = rng.float(0.2, 0.8) * SCALING
+			this.color = rng.int(0, 0xFFFFFF)
+			this.population = rng.int(10, 99)
+			this.selected = false
+			this.name = generatePlanetName([
+				1-(this.size-.2*SCALING)/(.8*SCALING-.2*SCALING),
+				(rgb2hsv(this.color) >> 16) / 0xFF,
+			])
+
+			this.graphicElements = {}
+		}
+		containsLocation(xy) {
+			const distance = Math.sqrt(Math.pow(this.x - xy.x, 2) + Math.pow(this.y - xy.y, 2))
+			return distance < this.size
+		}
+		distanceTo(planet) {
+			const distance = Math.sqrt(Math.pow(this.x - planet.x, 2) + Math.pow(this.y - planet.y, 2))
+			return distance - this.size - planet.size
+		}
+		addPopulation(amount) {
+			this.population += amount
+			const text = this.graphicElements.popText
+			if(text) {
+				text.text = this.population
+			}
+		}
+		render(graphics, time) {
+			graphics.beginFill(0x808080, this.selected ? 1 : .25)
+			graphics.drawCircle(this.x, this.y, this.size + BORDER_SIZE)
+			graphics.endFill()
+
+			graphics.beginFill(this.color)
+			graphics.drawCircle(this.x, this.y, this.size)
+			graphics.endFill()
+
+			const FONT_SIZE = 22
+
+			// Planet Name
+			if(this.graphicElements.nameBox == null) {
+				const box = new PIXI.Graphics()
+				box.x = this.x
+				box.y = this.y
+
+				const text = new PIXI.Text(this.name, {
+					fontFamily: 'Arial',
+					fontSize: FONT_SIZE,
+					align: 'left',
+					fill: '0xFFFFFF',
+				})
+				text.x = -text.width / 2
+				text.y = -text.height / 2
+
+				box.addChild(text)
+				graphics.addChild(box)
+				this.graphicElements.nameBox = box
+				this.graphicElements.nameText = text
+
+				if(text.width > text.height) {
+					box.height = this.size
+					box.width = text.width * box.height/text.height
+				} else {
+					box.width = this.size
+					box.height = text.height * box.width/text.width
+				}
+			}
+
+			// Planet Population
+			if(this.graphicElements.textBox == null) {
+				const box = new PIXI.Graphics()
+				box.x = this.x + this.size*Math.SQRT1_2
+				box.y = this.y + this.size*Math.SQRT1_2
+
+				const text = new PIXI.Text(this.population, {
+					fontFamily: 'Arial',
+					fontSize: FONT_SIZE,
+					align: 'left',
+					fill: '0xFFFFFF',
+				})
+				text.x = 0
+				text.y = 0
+
+				box.addChild(text)
+				graphics.addChild(box)
+				this.graphicElements.textBox = box
+				this.graphicElements.popText = text
+			}
+			this.graphicElements.textBox.height = FONT_SIZE*graphics.onePixel
+			this.graphicElements.textBox.width = this.graphicElements.popText.width * this.graphicElements.textBox.height/this.graphicElements.popText.height
+		}
 	}
 
 	return Planet
 })()
 
 var Ship = (function () {
-	function Ship(from, to, departureTime) {
+	function Ship(from, to, departureTime, population) {
 		this.fromPlanet = from
 		this.toPlanet = to
 		this.departureTime = departureTime
+		this.population = population
 
 		// Update from to be on the surface of the from planet
 		const angle = Math.atan2(this.toPlanet.y - this.fromPlanet.y, this.toPlanet.x - this.fromPlanet.x)
@@ -147,8 +247,6 @@ var Ship = (function () {
 		this.x = this.fromLoc.x
 		this.y = this.fromLoc.y
 		this.distance = 0
-
-		console.log(this)
 	}
 
 	/**
--- scripts/Viewport.js
+++ scripts/Viewport.js
@@ -2,155 +2,162 @@ var Viewport = (function() {
 	const ZOOM_SPEED = 16
 	const MAP_MOVE_SENSITIVITY = 1
 
-	const Viewport = function(divs, engine) {
-		const THIS_VIEWPORT = this
-		const ENGINE = engine
-		this.viewLocation = {x:0, y:0, zoom: 0}
+	class Viewport {
+		constructor(divs, engine) {
+			const THIS_VIEWPORT = this
+			const ENGINE = engine
+			this.viewLocation = { x: 0, y: 0, zoom: 0 }
 
-		let turn = 0 // int, +1 per turn
-		let ingameTime = 0 // +1 per turn; can use decimal for computing frames
-		let animationTime = 0 // +1 per ms
+			let turn = 0 // int, +1 per turn
+			let ingameTime = 0 // +1 per turn; can use decimal for computing frames
+			let animationTime = 0 // +1 per ms
 
-		const HTML = divs.viewport
-		const pixi = new PIXI.Application({ resizeTo: window, clearBeforeRender: true, antialias: true })
-		HTML.append(pixi.view)
-		this._div = HTML
+			const HTML = divs.viewport
+			const pixi = new PIXI.Application({ resizeTo: window, clearBeforeRender: true, antialias: true })
+			HTML.append(pixi.view)
+			this._div = HTML
 
-		const graphics = new PIXI.Graphics()
-		pixi.stage.addChild(graphics)
-		this.gameToViewportMatrix = new PIXI.Matrix()
+			const graphics = new PIXI.Graphics()
+			this.graphics = graphics
+			pixi.stage.addChild(graphics)
+			this.gameToViewportMatrix = new PIXI.Matrix()
 
-		let mouseLocation = {x:0, y:0}
-		let dragnDrop = false
+			let mouseLocationGame = { x: 0, y: 0 }
+			let dragnDrop = false
 
-		// Zoom on mouse wheel scroll
-		HTML.bind('mousewheel DOMMouseScroll', (e)=>{
-			const mouseLocationViewport = THIS_VIEWPORT.getViewportMouseLocation(e)
-			THIS_VIEWPORT.updateZoom(mouseLocationViewport, THIS_VIEWPORT.viewLocation.zoom + (e.originalEvent.wheelDelta || -e.originalEvent.detail) / ZOOM_SPEED)
-		}).bind('mousedown', (e)=>{
-			if(e.button === 2 && !dragnDrop) {
-				// Right click
-				HTML.css('cursor', 'move')
-				dragnDrop = mouseLocation
-			}
-		}).bind('mouseup', (e)=>{
-			if(e.button === 0) {
-				// Left click
-				engine.onClick(mouseLocation, ingameTime)
-			} else if(e.button == 2) {
-				// Right click
-				HTML.css('cursor', 'default')
-				dragnDrop = false
-				e.preventDefault()
-			}
-		}).bind('mousemove', (e)=>{
-			const mouseLocationViewport = THIS_VIEWPORT.getViewportMouseLocation(e)
-			mouseLocation = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
-			if(dragnDrop) {
-				if((e.buttons & 2) === 0) {
-					// Not rightclicking anymore; can happen if user releases the mouse button outside the window
+			// Zoom on mouse wheel scroll
+			HTML.bind('mousewheel DOMMouseScroll', (e) => {
+				const mouseLocationViewport = THIS_VIEWPORT.getViewportMouseLocation(e)
+				THIS_VIEWPORT.updateZoom(mouseLocationViewport, THIS_VIEWPORT.viewLocation.zoom + (e.originalEvent.wheelDelta || -e.originalEvent.detail) / ZOOM_SPEED)
+			}).bind('mousedown', (e) => {
+				if(e.button === 2 && !dragnDrop) {
+					// Right click
+					HTML.css('cursor', 'move')
+					dragnDrop = mouseLocationGame
+				}
+			}).bind('mouseup', (e) => {
+				if(e.button === 0) {
+					// Left click
+					engine.onClick(mouseLocationGame, ingameTime)
+				} else if(e.button == 2) {
+					// Right click
 					HTML.css('cursor', 'default')
 					dragnDrop = false
-				} else {
-					const dx = (mouseLocation.x - dragnDrop.x) * MAP_MOVE_SENSITIVITY
-					const dy = (mouseLocation.y - dragnDrop.y) * MAP_MOVE_SENSITIVITY
-					THIS_VIEWPORT.viewLocation.x -= dx
-					THIS_VIEWPORT.viewLocation.y -= dy
-					dragnDrop = {x: mouseLocation.x + dx, y: mouseLocation.y + dy}
-					THIS_VIEWPORT.updateView()
+					e.preventDefault()
+				}
+			}).bind('mousemove', (e) => {
+				const mouseLocationViewport = THIS_VIEWPORT.getViewportMouseLocation(e)
+				mouseLocationGame = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
+				if(dragnDrop) {
+					if((e.buttons & 2) === 0) {
+						// Not rightclicking anymore; can happen if user releases the mouse button outside the window
+						HTML.css('cursor', 'default')
+						dragnDrop = false
+					} else {
+						const dx = (mouseLocationGame.x - dragnDrop.x) * MAP_MOVE_SENSITIVITY
+						const dy = (mouseLocationGame.y - dragnDrop.y) * MAP_MOVE_SENSITIVITY
+						THIS_VIEWPORT.viewLocation.x -= dx
+						THIS_VIEWPORT.viewLocation.y -= dy
+						dragnDrop = { x: mouseLocationGame.x + dx, y: mouseLocationGame.y + dy }
+						THIS_VIEWPORT.updateView()
+					}
+				}
+			}).bind('contextmenu', (e) => {
+				e.preventDefault()
+			})
+
+			const onIngameTimeStop = function() {
+				divs['btn-nextTurn'].prop('disabled', false)
+			}
+
+			const onTick = function() {
+				const deltaS = pixi.ticker.deltaMS / 1000
+				animationTime += deltaS
+				if(ingameTime < turn) {
+					ingameTime += deltaS * ENGINE.TURNS_PER_SECOND
+					if(ingameTime > turn) {
+						ingameTime = turn
+						onIngameTimeStop()
+					}
+					divs['label-turn'].text(ingameTime.toFixed(2))
+				}
+
+				// Update game data
+				ENGINE.update(ingameTime)
+
+				// Redraw all (TODO later: instead of redrawing everything, add every sub elements as graphics.addChild(subElement), and let pixiJS to the job)
+				graphics.clear()
+				graphics.onePixel = 1 / graphics.scale.x
+				graphics.mouseLocation = mouseLocationGame
+				if(!ENGINE.render(graphics, animationTime)) {
+					console.error('Engine render failed: Ticker Stopped.')
+					pixi.ticker.stop()
 				}
 			}
-		}).bind('contextmenu', (e)=>{
-			e.preventDefault()
-		})
 
-		const onIngameTimeStop = function() {
-			divs['btn-nextTurn'].prop('disabled', false)
-		}
-		pixi.ticker.add(()=>{
-			const deltaS = pixi.ticker.deltaMS / 1000
-			animationTime += deltaS
-			if(ingameTime < turn) {
-				ingameTime += deltaS * ENGINE.TURNS_PER_SECOND
-				if(ingameTime > turn) {
-					ingameTime = turn
-					onIngameTimeStop()
+			pixi.ticker.add(onTick)
+
+			divs['btn-nextTurn'].click(() => {
+				turn += 1
+				divs['btn-nextTurn'].prop('disabled', true)
+			})
+
+
+			this.updateZoom = function(mouseLocationViewport, newZoom) {
+				// Where is the mouse in the game before zoom
+				const mouseLocationGameBefore = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
+
+				// Compute zoom value
+				THIS_VIEWPORT.viewLocation.zoom = newZoom
+
+				// Fix limits depending on engine bounds
+				const gameBounds = ENGINE.getBounds() // [{x,y}, {x,y}]
+				const viewportBounds = this.getViewportBoundsOnScreen() // [{x,y}, {x,y}]
+				const ratioX = (viewportBounds[1].x - viewportBounds[0].x) / (gameBounds[1].x - gameBounds[0].x)
+				const ratioY = (viewportBounds[1].y - viewportBounds[0].y) / (gameBounds[1].y - gameBounds[0].y)
+				const minZoom = Math.log2(Math.min(ratioX, ratioY) * .9) // Tolerate 5% margin on every side
+				if(this.viewLocation.zoom < minZoom) {
+					this.viewLocation.zoom = minZoom
 				}
-				divs['label-turn'].text(ingameTime.toFixed(2))
+
+				// Apply zoom & viewport move, to the matrix only
+				THIS_VIEWPORT.updateView(false)
+
+				// Where is the mouse in the game after zoom
+				const mouseLocationGameAfter = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
+
+				// Move the viewport so the mouse stays in the same place
+				THIS_VIEWPORT.viewLocation.x += mouseLocationGameAfter.x - mouseLocationGameBefore.x
+				THIS_VIEWPORT.viewLocation.y += mouseLocationGameAfter.y - mouseLocationGameBefore.y
+
+				// Apply to the matrix
+				THIS_VIEWPORT.updateView(true)
 			}
 
-			// Update game data
-			ENGINE.update(ingameTime)
-
-			// Redraw all (TODO later: instead of redrawing everything, add every sub elements as graphics.addChild(subElement), and let pixiJS to the job)
-			graphics.clear()
-			graphics.onePixel = 1/graphics.scale.x // this.gameToViewportMatrix.applyInverse({x:1, y:1}).x
-			if(!ENGINE.render(graphics, animationTime)) {
-				console.error('Engine render failed: Ticker Stopped.')
-				pixi.ticker.stop()
-			}
-		})
-
-		divs['btn-nextTurn'].click(()=>{
-			turn += 1
-			divs['btn-nextTurn'].prop('disabled', true)
-		})
-
-
-		this.updateZoom = function(mouseLocationViewport, newZoom) {
-			// Where is the mouse in the game before zoom
-			const mouseLocationGameBefore = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
-
-			// Compute zoom value
-			THIS_VIEWPORT.viewLocation.zoom = newZoom
-
-			// Fix limits depending on engine bounds
-			const gameBounds = ENGINE.getBounds() // [{x,y}, {x,y}]
-			const viewportBounds = this.getViewportBoundsOnScreen() // [{x,y}, {x,y}]
-			const ratioX = (viewportBounds[1].x - viewportBounds[0].x) / (gameBounds[1].x - gameBounds[0].x)
-			const ratioY = (viewportBounds[1].y - viewportBounds[0].y) / (gameBounds[1].y - gameBounds[0].y)
-			const minZoom = Math.log2(Math.min(ratioX, ratioY) * .9) // Tolerate 5% margin on every side
-			if(this.viewLocation.zoom < minZoom) {
-				this.viewLocation.zoom = minZoom
-			}
-
-			// Apply zoom & viewport move, to the matrix only
-			THIS_VIEWPORT.updateView(false)
-
-			// Where is the mouse in the game after zoom
-			const mouseLocationGameAfter = this.gameToViewportMatrix.applyInverse(mouseLocationViewport)
-
-			// Move the viewport so the mouse stays in the same place
-			THIS_VIEWPORT.viewLocation.x += mouseLocationGameAfter.x - mouseLocationGameBefore.x
-			THIS_VIEWPORT.viewLocation.y += mouseLocationGameAfter.y - mouseLocationGameBefore.y
-
-			// Apply to the matrix
-			THIS_VIEWPORT.updateView(true)
+			this.updateView(true)
 		}
-		this.updateView = function(doUpdate=true) {
+		updateView(applyUpdate = true) {
 			// Initiate the matrix
 			this.gameToViewportMatrix.identity()
 
 			// Apply zoom
-			const zoomFactor = 2**this.viewLocation.zoom
+			const zoomFactor = 2 ** this.viewLocation.zoom
 			this.gameToViewportMatrix.scale(zoomFactor, zoomFactor)
 			this.gameToViewportMatrix.translate(this.viewLocation.x * zoomFactor, this.viewLocation.y * zoomFactor)
 
-			if(doUpdate) {
-				graphics.transform.setFromMatrix(this.gameToViewportMatrix)
-				graphics.pixelSize
+			if(applyUpdate) {
+				this.graphics.transform.setFromMatrix(this.gameToViewportMatrix)
 			}
 		}
 
-		THIS_VIEWPORT.updateView(true)
+		getViewportBoundsOnScreen() {
+			return [{ x: this._div.offset().left, y: this._div.offset().top }, { x: this._div.offset().left + this._div.width(), y: this._div.offset().top + this._div.height() }]
+		}
+		getViewportMouseLocation(event) {
+			return { x: event.pageX - this._div.offset().left, y: event.pageY - this._div.offset().top }
+		}
 	}
 
-	Viewport.prototype.getViewportBoundsOnScreen = function() {
-		return [{x: this._div.offset().left, y: this._div.offset().top}, {x: this._div.offset().left + this._div.width(), y: this._div.offset().top + this._div.height()}]
-	}
-	Viewport.prototype.getViewportMouseLocation = function(event) {
-		return {x: event.pageX - this._div.offset().left, y: event.pageY - this._div.offset().top}
-	}
 
 	return Viewport
 })()
